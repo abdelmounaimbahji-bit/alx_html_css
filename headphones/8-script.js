e716c9ac2fc3dc30be4715d8ee51502609c14f1e
Abdelmounaim <abdelmounaimbahji@gmail.com>
2025-10-09 00:18:53 +0100

    Humberger Menu

---
+++ headphones/8-script.js
@@ -0,0 +1,7 @@
+
+const hamburgerDiv = document.getElementById('hamburger-btn')
+const menu = document.getElementById('hamburger-menu')
+
+hamburgerDiv.addEventListener('click', (e) => {
+    menu.setAttribute("hidden", "");
+})
